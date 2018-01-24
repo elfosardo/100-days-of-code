e69dbf3ab4f280ff7c5f3c5f78e25299bde5ab22
Riccardo Pittau <elfosardo@gmail.com>
2018-01-24 13:13:12 +0100

    playing with github apis

---
+++ github_tools/github_tools.py
@@ -0,0 +1,18 @@
+import argparse
+import getpass
+
+from github import Github
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Github tools')
+    parser.add_argument('username', metavar='U',
+                        help='your github username')
+    args = parser.parse_args()
+
+    user = args.username
+    password = getpass.getpass('Github Password: ')
+
+    g = Github(user, password)
+    for repo in g.get_user().get_repos():
+        print(repo.name)
