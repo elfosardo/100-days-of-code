14ced18bed68f47384be86a2ec305c4dcd07eb99
Riccardo Pittau <elfosardo@gmail.com>
2018-01-28 13:46:43 +0100

    [socket_server] some refactoring; adding options to specify port and file to transfer

--- socket_server/clients/elf_file_transfer_cli.py
+++ socket_server/clients/elf_file_transfer_cli.py
@@ -1,21 +1,27 @@
+import argparse
 import socket
 
-FILENAME = 'send_test.txt'
-PORT = 50000
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='File Transfer Client')
+    parser.add_argument('myfile', metavar='F',
+                        help='file to transfer')
+    parser.add_argument('-p', '--port', dest='port', default=50000,
+                        help='connection port; default 50000')
+    args = parser.parse_args()
 
-my_socket = socket.socket()
-host = socket.gethostname()
+    my_socket = socket.socket()
+    host = socket.gethostname()
 
-my_socket.connect((host, PORT))
-my_socket.send('Hey server!'.encode())
+    my_socket.connect((host, args.port))
+    my_socket.send('Hey server!'.encode())
 
-my_file_to_send = open(FILENAME, 'rb')
-data_to_send = my_file_to_send.read(1024)
-while data_to_send:
-    my_socket.send(data_to_send)
+    my_file_to_send = open(args.myfile, 'rb')
     data_to_send = my_file_to_send.read(1024)
-my_file_to_send.close()
+    while data_to_send:
+        my_socket.send(data_to_send)
+        data_to_send = my_file_to_send.read(1024)
+    my_file_to_send.close()
 
-print('File successfully sent')
-my_socket.close()
-print('Connection closed')
+    print('File successfully sent')
+    my_socket.close()
+    print('Connection closed')
--- socket_server/servers/elf_file_transfer.py
+++ socket_server/servers/elf_file_transfer.py
@@ -1,29 +1,35 @@
+import argparse
 import socket
 
-PORT = 50000
 
-my_socket = socket.socket()
-host = socket.gethostname()
-my_socket.bind((host, PORT))
-my_socket.listen(5)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='File Transfer Server')
+    parser.add_argument('-p', '--port', dest='port', default=50000,
+                        help='connection port; default 50000')
+    args = parser.parse_args()
 
-print('Server listening...')
+    my_socket = socket.socket()
+    host = socket.gethostname()
+    my_socket.bind((host, args.port))
+    my_socket.listen(5)
 
-while True:
-    (my_connection, addr) = my_socket.accept()
-    print('Got connection from', addr)
-    data = my_connection.recv(1024)
-    print('Connection received')
-    with open('received_file.txt', 'wb') as received_file:
-        print('Upload started')
-        while True:
-            print('Receiving data...')
-            data = my_connection.recv(1024)
-            if not data:
-                break
-            received_file.write(data)
-    received_file.close()
+    print('Server listening...')
 
-    print('Done receiving')
-    my_connection.send('Data received'.encode())
-    my_connection.close()
+    while True:
+        (my_connection, addr) = my_socket.accept()
+        print('Got connection from', addr)
+        data = my_connection.recv(1024)
+        print('Connection received')
+        with open('received_file.txt', 'wb') as received_file:
+            print('Upload started')
+            while True:
+                print('Receiving data...')
+                data = my_connection.recv(1024)
+                if not data:
+                    break
+                received_file.write(data)
+        received_file.close()
+
+        print('Done receiving')
+        my_connection.send('Data received'.encode())
+        my_connection.close()
