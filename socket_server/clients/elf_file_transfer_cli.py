464065d4b8218f0a4cffd5ec8991d0cab8ed6fe1
Riccardo Pittau <elfosardo@gmail.com>
2018-01-29 13:51:05 +0100

    [socket_server] sending the name of the file, destination file will have the same name as original; moving deafult port and buffer to static

--- socket_server/clients/elf_file_transfer_cli.py
+++ socket_server/clients/elf_file_transfer_cli.py
@@ -1,11 +1,14 @@
 import argparse
 import socket
 
+DEFAULT_PORT = 50000
+BUFFER = 1024
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='File Transfer Client')
     parser.add_argument('myfile', metavar='F',
                         help='file to transfer')
-    parser.add_argument('-p', '--port', dest='port', default=50000,
+    parser.add_argument('-p', '--port', dest='port', default=DEFAULT_PORT,
                         help='connection port; default 50000')
     args = parser.parse_args()
 
@@ -13,13 +16,15 @@ if __name__ == '__main__':
     host = socket.gethostname()
 
     my_socket.connect((host, args.port))
-    my_socket.send('Hey server!'.encode())
+
+    filename  = args.myfile + '\n'
+    my_socket.send(filename.encode())
 
     my_file_to_send = open(args.myfile, 'rb')
-    data_to_send = my_file_to_send.read(1024)
+    data_to_send = my_file_to_send.read(BUFFER)
     while data_to_send:
         my_socket.send(data_to_send)
-        data_to_send = my_file_to_send.read(1024)
+        data_to_send = my_file_to_send.read(BUFFER)
     my_file_to_send.close()
 
     print('File successfully sent')
--- socket_server/servers/elf_file_transfer.py
+++ socket_server/servers/elf_file_transfer.py
@@ -1,10 +1,13 @@
 import argparse
 import socket
 
+DEFAULT_PORT = 50000
+BUFFER = 1024
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='File Transfer Server')
-    parser.add_argument('-p', '--port', dest='port', default=50000,
+    parser.add_argument('-p', '--port', dest='port', default=DEFAULT_PORT,
                         help='connection port; default 50000')
     args = parser.parse_args()
 
@@ -13,18 +16,21 @@ if __name__ == '__main__':
     my_socket.bind((host, args.port))
     my_socket.listen(5)
 
+
     print('Server listening...')
 
     while True:
         (my_connection, addr) = my_socket.accept()
         print('Got connection from', addr)
-        data = my_connection.recv(1024)
+        data = my_connection.recv(BUFFER)
         print('Connection received')
-        with open('received_file.txt', 'wb') as received_file:
+        received_file_name = (data.decode()).splitlines()[0]
+        print('Receiving file: ', received_file_name)
+        with open(received_file_name, 'wb') as received_file:
             print('Upload started')
             while True:
                 print('Receiving data...')
-                data = my_connection.recv(1024)
+                data = my_connection.recv(BUFFER)
                 if not data:
                     break
                 received_file.write(data)
