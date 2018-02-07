51f8768f92e0faebc26582cb662c0110dc197ffc
Riccardo Pittau <elfosardo@gmail.com>
2018-02-07 23:12:54 +0100

    [runkeeper_scraper] moving get_total_activities into a function; removing parameter profile_name and getting it automatically from after login landing page; importing BeautifulSoup as BfS to respect PEP8 naming convention

--- runkeeper_scraper/runkeeper_scraper.py
+++ runkeeper_scraper/runkeeper_scraper.py
@@ -2,7 +2,7 @@ import argparse
 import getpass
 import requests
 import re
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup as BfS
 
 
 SITE_URL = 'https://runkeeper.com'
@@ -11,57 +11,46 @@ LOGIN_URL = 'https://runkeeper.com/login'
 
 def get_profile_name():
     my_home = '{}/home'.format(SITE_URL)
-    print(my_home)
     real_home = session.get(my_home)
-    soup = BS(real_home.text, 'html.parser')
-    profile_link = soup.find('a', {'href': re.compile(r"/user/[a-zA-Z0-9]*/profile")})
-#    profile_urlo = profile_link.text
-#    profile_name =
-    return profile_link
+    soup = BfS(real_home.text, 'html.parser')
+    profile_href = soup.find('a', {'href': re.compile(r"/user/[a-zA-Z0-9]*/profile")})
+    profile_path = profile_href.attrs['href']
+    profile_name = profile_path.split('/')[2]
+    return profile_name
+
+
+def get_total_activities():
+    request_url = "{}/user/{}/profile".format(SITE_URL, my_profile_name)
+    request = session.get(request_url)
+    html_code = request.text
+    soup = BfS(html_code, 'html.parser')
+    activities = soup.find('div', {'id': 'totalActivities'}).find('h1')
+    return activities
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get info from runkeeper site')
     parser.add_argument('email', metavar='email',
                         help='the username is your e-mail address')
-    parser.add_argument('profile_name', metavar='Profile Name',
-                        help='Name of the profile')
     args = parser.parse_args()
 
     my_password = getpass.getpass()
 
-    request_url = "https://runkeeper.com/user/{}/profile".format(args.profile_name)
-
-    print(request_url)
-
-#    payload = {
-#        'email': args.email,
-#        'password': my_password
-#    }
-
     with requests.Session() as session:
 
         login_page = session.get(LOGIN_URL)
-        my_soup = BS(login_page.text, 'html.parser')
+        my_soup = BfS(login_page.text, 'html.parser')
         login_form = my_soup.find_all('input', {'type': 'hidden'})
         hidden_payload = {}
         for element in login_form:
             hidden_payload[element.attrs['name']] = element.attrs['value']
         hidden_payload['email'] = args.email
         hidden_payload['password'] = my_password
-        #print(hidden_payload)
         post = session.post(LOGIN_URL, data=hidden_payload)
         cookie = post.cookies.get('checker')
-        print(cookie)
-        r = session.get(request_url)
-        #print(r.text)
-        html_code = r.text
-        my_soup = BS(html_code, 'html.parser')
-        #print(my_soup.prettify())
-        total_activities = my_soup.find('div', {'id': 'totalActivities'}).find('h1')
+
+        my_profile_name = get_profile_name()
+
+        total_activities = get_total_activities()
+
         print(total_activities.text)
-
-        profile_url = get_profile_name()
-        print(profile_url)
-
-
