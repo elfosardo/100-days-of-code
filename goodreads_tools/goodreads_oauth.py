d5dfaa6cf2afd1da85a23d4750aa3725dfbfc6a2
Riccardo Pittau <elfosardo@gmail.com>
2018-03-14 19:06:00 +0100

    day 57, authorizing token using Selenium, part 2

--- goodreads_tools/goodreads_oauth.py
+++ goodreads_tools/goodreads_oauth.py
@@ -5,39 +5,43 @@ import urllib.parse as up
 
 from selenium import webdriver
 
-url = cfg.API_URL
-request_token_url = '{}/oauth/request_token'.format(url)
-authorize_url = '{}/oauth/authorize'.format(url)
-access_token_url = '{}/oauth/access_token'.format(url)
 
-consumer = oauth2.Consumer(key=cfg.api_key,
-                           secret=cfg.api_secret)
+def authorize_token():
+    driver = webdriver.Firefox()
+    driver.get(authorize_link)
+    assert 'Sign in' in driver.title
+    username_elem = driver.find_element_by_id('user_email')
+    username_elem.clear()
+    username_elem.send_keys(cfg.user_email)
+    password_elem = driver.find_element_by_id('user_password')
+    password_elem.clear()
+    password_elem.send_keys(my_password)
+    remember_me_elem = driver.find_element_by_id('remember_me')
+    remember_me_elem.click()
+    driver.find_element_by_name('next').click()
+    driver.find_element_by_xpath("//input[@name='commit' and @value='Allow access']").click()
 
-client = oauth2.Client(consumer)
 
-response, content = client.request(request_token_url, 'GET')
-if response['status'] != '200':
-    raise Exception('Invalid response: {}, content: '.format(response['status'] + content))
+if __name__ == '__main__':
+    url = cfg.API_URL
+    request_token_url = '{}/oauth/request_token'.format(url)
+    authorize_url = '{}/oauth/authorize'.format(url)
+    access_token_url = '{}/oauth/access_token'.format(url)
 
-request_token = dict(up.parse_qsl(content.decode('utf-8')))
+    consumer = oauth2.Consumer(key=cfg.api_key,
+                               secret=cfg.api_secret)
 
-authorize_link = '{}?oauth_token={}'.format(authorize_url, request_token['oauth_token'])
-print('Authorizing token using Selenium driver')
-print(authorize_link)
-my_password = getpass.getpass('Goodreads Password: ')
+    client = oauth2.Client(consumer)
 
-driver = webdriver.Firefox()
-driver.get(authorize_link)
-assert 'Sign in' in driver.title
-username_elem = driver.find_element_by_id('user_email')
-username_elem.clear()
-username_elem.send_keys(cfg.user_email)
+    response, content = client.request(request_token_url, 'GET')
+    if response['status'] != '200':
+        raise Exception('Invalid response: {} {}, content: '.format(response['status'], content))
 
-password_elem = driver.find_element_by_id('user_password')
-password_elem.clear()
-password_elem.send_keys(my_password)
+    request_token = dict(up.parse_qsl(content.decode('utf-8')))
 
-remember_me_elem = driver.find_element_by_id('remember_me')
-remember_me_elem.click()
+    authorize_link = '{}?oauth_token={}'.format(authorize_url, request_token['oauth_token'])
+    print('Authorizing token using Selenium driver')
+    print(authorize_link)
+    my_password = getpass.getpass('Goodreads Password: ')
 
-driver.find_element_by_name('next').click()
+    authorize_token()
