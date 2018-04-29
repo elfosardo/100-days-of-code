5b8c0ffe82668a28a06714cd2647c72627c959b7
Riccardo Pittau <elfosardo@gmail.com>
2018-04-29 14:03:51 +0200

    [github_tools] refactoring

--- github_tools/github_tools.py
+++ github_tools/github_tools.py
@@ -6,7 +6,7 @@ from github import Github
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Github tools')
-    parser.add_argument('username', metavar='U',
+    parser.add_argument('username',
                         help='your github username')
     args = parser.parse_args()
 
@@ -14,8 +14,9 @@ if __name__ == '__main__':
     password = getpass.getpass('Github Password: ')
 
     g = Github(user, password)
+    print('{:<25} {:<6} {}'.format('Name', 'Stars', 'Watchers'))
     for repo in g.get_user().get_repos():
         name = repo.name
         stars = repo.stargazers_count
         watchers = repo.watchers_count
-        print('{:<25} {} {}'.format(name, stars, watchers))
+        print('{:<25} {:<6} {}'.format(name, stars, watchers))
