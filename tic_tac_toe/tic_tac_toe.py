1245dab74e81f73f3b0d3cca3e23e9c1fbea7c2c
Riccardo Pittau <elfosardo@gmail.com>
2018-05-18 16:12:50 +0200

    adding winning conditions

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -68,6 +68,22 @@ def get_computer_move(board):
 
 
 def check_for_winner(board):
+    symbols = ['X', 'Y']
+    victory_lines = [
+        [board[0][0], board[0][1], board[0][2]],  # row1
+        [board[1][0], board[1][1], board[1][2]],  # row2
+        [board[2][0], board[2][1], board[2][2]],  # row3
+        [board[0][0], board[1][0], board[2][0]],  # column1
+        [board[0][1], board[1][1], board[2][1]],  # column2
+        [board[0][2], board[1][2], board[2][2]],  # column3
+        [board[0][0], board[1][1], board[2][2]],  # diag1
+        [board[2][0], board[1][1], board[0][2]]  # diag2
+    ]
+    for line in victory_lines:
+        if ' ' not in line:
+            print(line)
+            if line.count(line[0]) == len(line):
+                return line[0]
     return False
 
 
